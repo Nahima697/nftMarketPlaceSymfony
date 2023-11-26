52aba37db71fb11b2d9d1f6a01e3074615955811
Nahima697 <nahima.toumi697@gmail.com>
2023-11-26 10:49:02 +0100

    push avant deployement

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Nelmio\SecurityBundle\NelmioSecurityBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
